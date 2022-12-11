ce6706f5dbe04b300db1192f1838ca67b0601452
Ricardo <ricardo8arr@gmail.com>
2022-12-10 19:44:09 -0600

    first commit

---
+++ pages/nosotros.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+function Nosotros() {
+    return (
+        <>
+            <h1>Nosotros</h1>
+            <Link href="/">Ir a la página principal</Link>
+        </>
+    )
+}
+
+export default Nosotros
